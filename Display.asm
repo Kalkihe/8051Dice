0f954207dbc11a2f01f1d515f3b59f52a131cd42
SlaxXxX <janis.schneider97@web.de>
2019-05-16 11:46:41 +0200

    Display should work now. To use, put dice value in R7 and call display.

--- Display.asm
+++ Display.asm
@@ -1,1 +1,15 @@
-;File for display routines
+display:
+mov DPTR, #table
+mov a, R7
+movc a, @a+dptr
+mov P3, a
+ret
+
+org 300h
+table:
+db 11000000b
+db 11111001b, 10100100b, 10110000b
+db 10011001b, 10010010b, 10000010b
+db 11111000b, 10000000b, 10010000b
+
+end
