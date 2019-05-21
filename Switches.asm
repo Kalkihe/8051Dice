f7327ec1a812fce4bbe1a9028d642ffaa8b6358c
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-05-21 12:46:57 +0200

    added mode determination bits

--- Switches.asm
+++ Switches.asm
@@ -2,41 +2,42 @@
 
 initSwitches:
 ;only once
-mov R4.0,0
-mov R4.1,0
-mov R4.2,0
+mov R4,#00h
 ret
 
 switchDetermination:
 ;call this everytime after you called "init" once
-mov A,P0.0 ;P0.0 is the pin for switch1
+mov A,P0.0 		;P0.0 is the pin for switch1
 cjne A,R4.0,switch1
-mov A,P0.1 ;P0.1 is the pin for switch2
+mov A,P0.1 		;P0.1 is the pin for switch2
 cjne A,R4.1,switch2
-mov A,P0.2 ;P0.2 is the pin for switch3
+mov A,P0.2 		;P0.2 is the pin for switch3
 cjne A,R4.2,switch3
 ret
 
 saveSwitchStates:
-mov R4.0,P0.0 ;save current state of switch1
-mov R4.1,P0.1 ;save current state of switch2
-mov R4.2,P0.2 ;save current state of switch3
+mov R4.0,P0.0 		;save current state of switch1
+mov R4.1,P0.1 		;save current state of switch2
+mov R4.2,P0.2 		;save current state of switch3
 ret
 
 switch1:
 ;0-9 dice
 jmp saveSwitchStates
-jmp dice1 ;placeholder
+mov R4.6,0		;last 2 bits used for setting used dice-mode
+mov R4.7,1		;set mode bits to "01" (1) for 0-9 dice
 ret
 
 switch2:
 ;1-6 dice
 jmp saveSwitchStates
-jmp dice2 ;placeholder
+mov R4.6,1		;last 2 bits used for setting used dice-mode
+mov R4.7,0		;set mode bits to "10" (2) for 1-6 dice
 ret
 
 switch3:
 ;8051 dice
 jmp saveSwitchStates
-jmp dice3 ;placeholder
+mov R4.6,1		;last 2 bits used for setting used dice-mode
+mov R4.7,1		;set mode bits to "11" (3) for 8051 dice
 ret
