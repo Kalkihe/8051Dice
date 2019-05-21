cfedd5699aec2925d5ef88241634d966a4fadd5c
CClavius <falko.reinert@gmail.com>
2019-05-21 15:27:08 +0200

    Update Random.asm

--- Random.asm
+++ Random.asm
@@ -1,36 +1,63 @@
-;File for generating random numbers 0-9+
+;;;  Zufallszahlengenerator + CASE-Anweisung + SATISTIK ----
+;;;  Zufallszahl in R2 E {0,...,7}
+;;;  Statistik in 030h - 038h 
+;;;
+;;EQU	ZUF8R, 0x20		;ein byte
+RNDM EQU 51h
+CSEG At 0H
+jmp init
+ORG 100H
 
-RNDMSEED EQU 50h
-RNDM EQU R7
-
-CSEG at 0h
-LJMP init
-;-----------------------------
-; Interupt-Einsprung Adressen
-
-ORG 0Bh
-INC RNDM		; Generierung Startwert für Zufallszahl
-RETI
-
-;-----------------
-; Initialisierung
-;-----------------
-ORG 0100h
+;-----------MAIN-----------------------------------
 init:
-MOV RNDM, #0d		; Startwert für Zufallszahlen mit 0 initialisieren
+ANF:
+;-----------GENERIER EINE ZUFALLSZAHL----------
+	 call ZUFALL         ;Zufallszahl A bestimmen zwischen 00h und ffh
+;----------- CASE-ANWEISUNG-------------------------
+         mov R7,#00h        ;Zähler initialisieren mit 0 
+neu:	 add A,#020h        ;die Zufallszahl plus 32 
+         inc R7            ;Zähler um 1 erhöhen
+	 jnc neu           ;falls schon Überlauf, dann weiter - sonst  addiere 32
+	
+	 mov A, R7          ;schreib Zahl in A
 
-;-----------------------------------------------------------------------------
-; Unterprogramm um eine (Pseudo-)Zufallszahl aus einem Startwert zu Berechnen
-;-----------------------------------------------------------------------------
-zufall:
-MOV A, RNDM		; initialisiere A mit R0
-JNZ zub			; Startwert darf nicht 0 sein
-CPL A			; complementiere, falls doch 0
-MOV RNDM, A
-zub:
-ANL A, #10111000b	;
-MOV C, P		;
-MOV A, RNDM		;
-RLC A			;
-MOV RNDM, A		;
-RET
+;--------Kontrolle/Statistik------------
+        cjne A,#01h, keine1
+        inc 0x30
+        jmp ANF 
+keine1:
+        cjne A,#02h, keine2
+        inc 0x31
+        jmp ANF 
+keine2:  cjne A,#03h, keine3
+        inc 0x32
+        jmp ANF  
+keine3:  cjne A,#04h, keine4
+        inc 0x33
+        jmp ANF  
+keine4:  cjne A,#05h, keine5
+        inc 0x34
+        jmp ANF 
+keine5:
+        cjne A,#06h, keine6
+        inc 0x35
+        jmp ANF 
+keine6:  cjne A,#07h, keine7
+        inc 0x36
+        jmp ANF  
+keine7:  inc 0x37
+        jmp ANF                     ;von vorn!
+;--------------------------------------------------
+
+; ------ Zufallszahlengenerator-----------------
+ZUFALL:	mov	A, RNDM   ; initialisiere A mit ZUF8R
+	jnz	ZUB
+	cpl	A
+	mov	RNDM, A
+ZUB:	anl	a, #10111000b
+	mov	C, P
+	mov	A, RNDM
+	rlc	A
+	mov	RNDM, A
+	ret
+end
